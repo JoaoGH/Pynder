dfdc4a22404b7118544bafefee2da9b5de7a946f
João <gabrielhartmannqwert@gmail.com>
2018-11-21 10:48:19 -0200

    tela de login

---
+++ main.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from tkinter import *
+janela = Tk()
+janela.title("Pynder")
+janela.geometry("400x700")
+xx=400
+yy=700
+fontGrande = ('',30)
+fontePequena = ('',15)
+
+
+def login():
+    print(nome.get())
+
+lb = Label(janela, text="Bem vindo", font=fontGrande).place(x=125, y=100)
+lb = Label(janela, text="Nome:", font=fontePequena).place(x=80, y=300)
+nome = Entry(janela, width=15, font=fontePequena)
+nome.place(x=150, y=300)
+
+bt = Button (janela, text="Entrar", font=fontePequena, command=login)
+bt.place(x=180, y=500)
+
+janela.mainloop()
