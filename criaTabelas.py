083c73fb96077fb64b52d398b4af75f8be2fcdff
Gabriel <tutogames730@gmail.com>
2018-11-21 10:13:54 -0200

    Cria tabelas

---
+++ criaTabelas.py
@@ -0,0 +1,15 @@
+from pymysql import *
+
+##banco: Pynder
+##tabela: score
+##campos: id,nome,score
+
+con = connect("localhost", "root", "", "Pynder")
+
+cur = con.cursor()
+
+sql= "CREATE TABLE score (id INT AUTO_INCREMENT PRIMARY KEY, nome VARCHAR(225), score INT)"
+cur.execute(sql)
+
+con.commit()
+con.close()
