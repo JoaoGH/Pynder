94581693e2d8560b7febe74c899761790e3466b2
Gabriel <tutogames730@gmail.com>
2018-11-21 14:42:00 -0200

    Ajustado para funcionar OO no GlobalVAriable

--- GlobalVariable.py
+++ GlobalVariable.py
@@ -1,15 +1,16 @@
-def __init__(self,nome,score):
-    self.nome = nome
-    self.score = score
+class GlobalVariable():
+    def __init__(self,nome,score):
+        self.nome = nome
+        self.score = score
 
-def setNome(self,nome):
-    self.nome =nome
+    def setNome(self, nome):
+        self.nome = nome
 
-def getNome(self):
-    return self.nome
+    def getNome(self):
+        return self.nome
 
-def setScore(self,score):
-    self.score = score
+    def setScore(self,score):
+        self.score = score
 
-def getScore(self):
-    return self.score
+    def getScore(self):
+        return self.score
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from tkinter import *
+import GlobalVariable
+
 janela = Tk()
 janela.title("Pynder")
 janela.geometry("400x700")
